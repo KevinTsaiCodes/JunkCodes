0f91085558232551e462efe90e35ef19c1a1a8a8
KevinTsai <53148219+KevinTsaiCodes@users.noreply.github.com>
2020-05-25 21:21:12 +0800

    Create p02_print_writer.java

---
+++ Java/AU/xmhuang/10801/Practice/p02_print_writer.java
@@ -0,0 +1,26 @@
+package Practice;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+
+
+public class p02_print_writer {
+	public static void main(String[] args) throws FileNotFoundException {
+		String path = "pokemon_data.csv";
+		File file = new File(path);
+		System.out.println(file.exists());
+		PrintWriter pw = null;
+		if(!file.exists()) { // 不管有無檔案都會幫你建立檔案
+			pw = new PrintWriter(new OutputStreamWriter(new FileOutputStream(file, true)));
+		/*  pw = new PrintWriter(new OutputStreamWriter(new FileOutputStream(file, true)));
+			表示檔案不會因每次跑 JVM 而覆蓋原本檔案內容，預設為 false，表示會被覆蓋
+		*/
+		}
+		pw = new PrintWriter(new OutputStreamWriter(new FileOutputStream(file, true)));
+		pw.write("Today"+","+"Yesterday"+","+"Tomorrow"+"\n");
+		pw.flush(); // 清除緩衝區資料，也就是IO
+		pw.close();
+	}
+}
