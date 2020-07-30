031ca84e40e163c37311d27620ef779573a02f7a
KevinTsai <53148219+KevinTsaiCodes@users.noreply.github.com>
2020-07-30 20:27:10 +0800

    Update p02_constructor.py

--- Python 基礎教學/OOP/p02_constructor.py
+++ Python 基礎教學/OOP/p02_constructor.py
@@ -1,5 +1,5 @@
 class Car:
-	def __init__(self, speed, color): # init method is a constructor, initialize the member
+	def __init__(self, speed, color): # init method is a constructor, initialize the member, 建構子
 		print('the __init__ is called')
 		print(color)
 		print(speed)
@@ -9,4 +9,13 @@ ford = Car(200, 'red') # automatic call the method
 # ford = Car() <-- error: TypeError: __init__() missing 2 required positional arguments: 'speed' and 'color'
 """
 def __init__(self, member_name_1, member_name_2, member_name_3)
+"""
+
+"""
+這有點類似Java這樣寫法,
+public class Student{
+	public Student(){ # 這裡等同於Python 的 def __init__ 方法
+	
+	}
+}
 """
