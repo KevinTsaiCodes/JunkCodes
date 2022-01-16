b0de578aac8a03a8fe9fb409326e3eab48ff5c9b
Kevin (Wei-Chun) Tsai <53148219+KevinTsaiCodes@users.noreply.github.com>
2022-01-16 23:07:01 +0800

    Create 01_basic.py

---
+++ Matplotlib/01_basic.py
@@ -0,0 +1,10 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+if __name__=='__main__':
+    plt.plot([1,2,3,4])
+    plt.ylabel('y label') # create y label name
+    plt.xlabel('x label') # create x label name
+    plt.title('Title of the plot') # make title
+    plt.show()
